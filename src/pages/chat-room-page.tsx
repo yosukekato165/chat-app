403c9aeb1818c831c29df482739c0f06e21317e2
yosukekato165 <padyk165@gmail.com>
2021-11-24 23:42:38 +0900

    fifth commit

--- src/pages/chat-room-page.tsx
+++ src/pages/chat-room-page.tsx
@@ -1,10 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { connect } from "react-redux";
+
 import UserList, { UserItem } from "../components/user-list";
 import Button from "../components/button";
 import MessageList, { MessageItem } from "../components/message-list";
 import JoinChatRoom from "../components/join-chat-room";
 import CreateMessage from "../components/create-message";
+import { ApplicationState } from "../store";
+import {
+  getCurrentRoom,
+  connectWebSocket,
+  joinRoom,
+  sendMessage,
+  clearCurrentRoom,
+  setCurrentRoomId,
+  leaveRoom,
+} from "../store/actions";
+
+interface ChatRoomPageProps {
+  // 現在のチャットルームID
+  currentRoomId: string;
+  // 現在のチャットルーム名
+  currentRoomName: string;
+  // 現在のチャットルームのユーザー⼀覧
+  currentRoomUsers: UserItem[];
+  // 現在のチャットルームのメッセージ⼀覧
+  currentRoomLogs: MessageItem[];
+  // WebSocketへの接続
+  connectWebSocket: typeof connectWebSocket;
+  // 現在のチャットルームの情報取得
+  getCurrentRoom: typeof getCurrentRoom;
+  // チャットルームへの⼊室
+  joinRoom: typeof joinRoom;
+  // メッセージの送信
+  sendMessage: typeof sendMessage;
+  // 現在のチャットルーム情報のクリア
+  clearCurrentRoom: typeof clearCurrentRoom;
+  // 現在のチャットルームID設定
+  setCurrentRoomId: typeof setCurrentRoomId;
+  // チャットルーム退室
+  leaveRoom: typeof leaveRoom;
+}
+
 const users: UserItem[] = [
   {
     name: "ユーザー1",
@@ -44,6 +82,26 @@ interface ChatRoomPageProps {}
 
 const ChatRoomPage = (props: ChatRoomPageProps) => {
   const [isJoin, setIsJoin] = useState(false);
+  const [currentRoomId, setCurrentRoomId] = useState("");
+  const [text, setText] = useState("");
+  const [userName, setUsername] = useState("");
+
+  useEffect(() => {
+    if (!currentRoomId) {
+      // 現在のチャットルームIDが未設定の場合はチャットルーム⼀覧に戻る
+      props.history.push("/");
+    } else {
+      // 現在のチャットルームIDが存在する場合は保存
+      props.setCurrentRoomId(currentRoomId);
+    }
+    // WebSocketへの接続
+    props.connectWebSocket();
+    setTimeout(() => {
+      // 現在のチャットルームの情報取得
+      this.getCurrentRoomInfo();
+    }, 1000);
+  });
+
   return (
     <ChatRoomPageStyle isJoin={isJoin}>
       <div className="menu">
